da144b503dbd3e3d43570062020e3fb4f3d015ed
ffffyuan <2637754948@qq.com>
2026-04-09 14:49:52 +0800

    111

--- server/db.js
+++ server/db.js
@@ -1,9 +1,24 @@
-import Database = require("better-sqlite3")
+const Database = require("better-sqlite3");
 
 let dbInstance = null;
 
-function getDB(){
-  if(!dbInstance){
-    
+function getDB() {
+  if (!dbInstance) {
+    dbInstance = new Database("./store/game.db");
+
+    dbInstance.exec(`
+      CREATE TABLE IF NOT EXISTS player_data (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      player_id TEXT UNIQUE NOT NULL,
+      json_data TEXT NOT NULL
+
+      CREATE TABLE IF NOT EXISTS inventory (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      player_id TEXT UNIQUE NOT NULL,
+      json_data TEXT NOT NULL
+    );
+      `);
   }
+  return dbInstance;
 }
+module.exports = getDB;
